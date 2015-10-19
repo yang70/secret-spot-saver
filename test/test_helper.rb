d80ae18951ce9559afd55bbf11a6d373f540648e
Matthew Yang <yang70@gmail.com>
2015-10-19 14:01:15 -0700

    add minitest reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 Rails.env = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
+require "minitest/reporters"
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class ActiveSupport::TestCase
   fixtures :all
