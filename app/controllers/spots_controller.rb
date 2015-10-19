4ad35346103d47aeb93ddc3f706332210ddebbbd
Matthew Yang <yang70@gmail.com>
2015-10-19 14:04:22 -0700

    passing create test for spots

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,2 +1,16 @@
 class SpotsController < ApplicationController
+  def create
+    spot = Spot.new(spot_params)
+    if spot.save
+      render json: spot, status: 201, location: spot
+    else
+      render json: spot.errors, status: 422
+    end
+  end
+
+  private
+
+  def spot_params
+    params.require(:spot).permit(:name, :lat, :lon, :water_type, :technique, :notes)
+  end
 end
