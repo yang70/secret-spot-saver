57101224519fc8d9ddf0de29555a5a30b22e1358
Matthew Yang <yang70@gmail.com>
2015-11-08 13:16:34 -0800

    change to array

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default to: Proc.new { @user.email, 'matt@matthewgyang.com'},
+  default to: Proc.new { [@user.email, 'matt@matthewgyang.com'] },
     :from => 'matt@matthewgyang.com'
 
   def new_welcome_email(user)
